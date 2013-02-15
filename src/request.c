ee818a5ecfcfb5946fc7fae543520b1e06f9edad
TonySeek <tonyseek@gmail.com>
2013-02-15 16:20:41 +0800

    Drop this.

--- src/request.c
+++ src/request.c
@@ -1,3 +1,5 @@
+#include <stddef.h>
+#include <stdlib.h>
 #include "request.h"
 #include "string.h"
 
@@ -13,8 +15,17 @@ void clean_header(struct header *h)
     clean_string(&h->value);
 }
 
-void init_header_list(struct header_list *hl)
+void init_header_list(struct header_list *hl, struct header *h)
 {
+    hl->item = *h;
+    hl->next = NULL;
+}
+
+void append_header_list(struct header_list *hl, struct header *h)
+{
+    while (hl->next)
+        hl = &hl->next;
+    init_header_list(&hl->next, h);
 }
 
 void clean_header_list(struct header_list *hl)
--- src/request.h
+++ src/request.h
@@ -11,19 +11,20 @@ struct header
 
 struct header_list
 {
-    struct header *item;
-    struct header_list *next;
+    struct header item;
+    struct header_list next;
 };
 
 struct request
 {
-    struct header_list *headers;
+    struct header_list headers;
     struct string body;
 };
 
 extern void init_header(struct header *);
 extern void clean_header(struct header *);
-extern void init_header_list(struct header_list *);
+extern void init_header_list(struct header_list *, struct header *);
+extern void append_header_list(struct header_list *, struct header *);
 extern void clean_header_list(struct header_list *);
 extern void init_request(struct request *);
 extern void clean_request(struct request *);
